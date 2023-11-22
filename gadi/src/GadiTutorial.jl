09be5d4e07766310d4c152b2166b798964c57029
Alberto F. Martin <alberto.martin@monash.edu>
2023-11-23 08:29:55 +1100

    Removing fixes

--- gadi/src/GadiTutorial.jl
+++ gadi/src/GadiTutorial.jl
@@ -11,7 +11,6 @@ const OPTIONS_CG_JACOBI = "-pc_type jacobi -ksp_type cg -ksp_converged_reason -k
 const OPTIONS_CG_AMG = "-pc_type gamg -ksp_type cg -ksp_converged_reason -ksp_rtol 1.0e-10"
 const OPTIONS_MUMPS = "-pc_type lu -ksp_type preonly -ksp_converged_reason -pc_factor_mat_solver_type mumps"
 
-include("fixes.jl")
 include("poisson_driver.jl")
 include("ins_driver.jl")
 
