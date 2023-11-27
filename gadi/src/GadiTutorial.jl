a011a6d8c278aefac57b3b9059bee25bc0c9fb9e
JordiManyer <j.manyer@hotmail.com>
2023-11-27 16:43:23 +1100

    AMR working in gadi

--- gadi/src/GadiTutorial.jl
+++ gadi/src/GadiTutorial.jl
@@ -4,12 +4,13 @@ using DrWatson
 using Gridap, GridapDistributed, GridapPETSc, GridapGmsh, GridapP4est
 using PartitionedArrays
 
-export OPTIONS_CG_JACOBI, OPTIONS_CG_AMG, OPTIONS_MUMPS, OPTIONS_NEUTON_MUMPS
-export main_poisson, main_ins
+export OPTIONS_CG_JACOBI, OPTIONS_CG_AMG, OPTIONS_MUMPS, OPTIONS_NEUTON_MUMPS, OPTIONS_MINRES
+export main_poisson, main_ins, main_amr
 
 const OPTIONS_CG_JACOBI = "-pc_type jacobi -ksp_type cg -ksp_converged_reason -ksp_rtol 1.0e-10"
 const OPTIONS_CG_AMG = "-pc_type gamg -ksp_type cg -ksp_converged_reason -ksp_rtol 1.0e-10"
 const OPTIONS_MUMPS = "-pc_type lu -ksp_type preonly -ksp_converged_reason -pc_factor_mat_solver_type mumps"
+const OPTIONS_MINRES = "-ksp_type minres -ksp_converged_reason -ksp_rtol 1.0e-10"
 
 include("fixes.jl")
 include("poisson_driver.jl")
